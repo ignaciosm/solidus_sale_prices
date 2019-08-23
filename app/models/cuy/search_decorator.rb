e76b5a5ec201e00b06e3562d9b52332252a64915
ignaciosm <ignaciosm@gmail.com>
2019-08-23 17:39:03 -0500

    fix search from /ofertas

--- app/models/cuy/search_decorator.rb
+++ app/models/cuy/search_decorator.rb
@@ -10,13 +10,12 @@ module Spree
         attr_accessor :current_user
         attr_accessor :pricing_options
 
-        def retrieve_products
+        def retrieve_products_onsale
           @products = get_base_scope
           curr_page = @properties[:page] || 1
 
           unless Spree::Config.show_products_without_price
-            #@products = @products.joins(:prices).merge(Spree::Price.where(pricing_options.search_arguments)).distinct    >>>>CAMBIADO POR IGNACIO
-            @products = @products.joins(:stock_items).where("count_on_hand > ? OR #{Spree::Variant.table_name}.track_inventory = ? OR backorderable = ?", 0, false, true).distinct
+            @products = @products.joins(:sale_prices).where('spree_sale_prices.value is not null').where('enabled =?', true).where('(start_at <= ? OR start_at IS NULL) AND (end_at >= ? OR end_at IS NULL)', Time.now, Time.now).joins(:stock_items).where("count_on_hand > ? OR #{Spree::Variant.table_name}.track_inventory = ? OR backorderable = ?", 0, false, true).distinct
           end
           @products = @products.page(curr_page).per(@properties[:per_page])
         end
